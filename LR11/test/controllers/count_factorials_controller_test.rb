25a2d2a9bc67b2b6f3d506222cd508ea8895f6cc
Alexey Zotov <zotov.alexei.1@yandex.ru>
2021-01-14 16:29:10 +0400

    LR11 test fix

--- LR11/test/controllers/count_factorials_controller_test.rb
+++ LR11/test/controllers/count_factorials_controller_test.rb
@@ -2,20 +2,20 @@ require 'test_helper'
 
 class CountFactorialsControllerTest < ActionDispatch::IntegrationTest
   test "should get input" do
-    get count_factorials_input_url
+    get input_url
     assert_response :success
   end
 
   test "should get view" do
-    get count_factorials_view_url
+    get view_url
     assert_response :success
   end
 
   test 'results are different for different inputs' do
-    get count_factorials_view_url, params: {max_n: 3}
+    get view_url, params: {max_n: 3}
     first_response = response.body
 
-    get count_factorials_view_url, params: {max_n: 6}
+    get view_url, params: {max_n: 6}
     second_response = response.body
 
     assert_not_equal first_response, second_response
--- LR8/config/routes.rb
+++ LR8/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root :to => redirect('/count_factorials/input')
+  root to: 'count_factorials#input'
   get 'count_factorials/input'
   get 'count_factorials/view'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
